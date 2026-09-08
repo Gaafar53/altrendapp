e930b7e0a9406f7ab4caf586f5704dd4ff9634e9
gomr996 <gomr996@github.com>
2026-09-08 19:16:51 +0300

    fix vite outDir to root dist

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   publicDir: path.resolve(__dirname, "client", "public"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
   server: {
     host: '0.0.0.0',
     port: 5174,
