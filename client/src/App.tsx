857254a46e22d8f3d4af6aa0cb4661763fb1f330
gomr996 <gomr996@github.com>
2026-09-08 19:21:45 +0300

    make Feed the homepage - mini internet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -52,7 +52,7 @@ function Router() {
   return (
     <Suspense fallback={<LoadingFallback />}>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route path="/" component={Feed} />
         <Route path="/upload" component={Upload} />
         <Route path="/video/:id" component={VideoDetail} />
         <Route path="/trending" component={Trending} />
